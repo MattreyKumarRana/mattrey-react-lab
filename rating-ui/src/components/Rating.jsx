8cdde86a5b9bec615569cde035c75694ebd02844
Mattrey Kumar Rana <mrana23@myseneca.ca>
2026-01-10 20:31:31 -0500

    Lists added to the rating ui project

---
+++ rating-ui/src/components/Rating.jsx
@@ -0,0 +1,20 @@
+import "../../src/index.css";
+
+const Rating = () => {
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+
+  return (
+    <div className="rating-container">
+      <h2>Rate your experience</h2>
+      <div className="stars">
+        {stars.map((star) => (
+          <span id={star} className="star">
+            {"\u2605"}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Rating;
