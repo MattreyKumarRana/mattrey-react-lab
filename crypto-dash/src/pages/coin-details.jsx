65d256788291c7524706483d5f10f9ddbeebc880
Mattrey Kumar Rana <mrana23@myseneca.ca>
2026-01-31 16:57:56 -0500

    Loading Spinner added to the crypto dash project

--- crypto-dash/src/pages/coin-details.jsx
+++ crypto-dash/src/pages/coin-details.jsx
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router";
+import {
+  Link,
+  UNSAFE_getTurboStreamSingleFetchDataStrategy,
+  useParams,
+} from "react-router";
+import Spinner from "../components/Spinner";
+
 const API_URL = import.meta.env.VITE_COIN_API_URL;
 
 const CoinDetailsPage = () => {
@@ -34,7 +40,7 @@ const CoinDetailsPage = () => {
         {coin ? `${coin.name} (${coin.symbol.toUpperCase()})` : "Coin Details"}
       </h1>
 
-      {loading && <p>Loading...</p>}
+      {loading && <Spinner />}
       {error && <div className="error">❌ {error}</div>}
 
       {!loading && !error && (
