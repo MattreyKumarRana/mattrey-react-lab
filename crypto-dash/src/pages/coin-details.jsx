9dac21ae11f16dc4b669ab116b90b258cf1bb36b
Mattrey Kumar Rana <mrana23@myseneca.ca>
2026-02-01 20:48:14 -0500

    Coin Chart Added to the crypto dash project

--- crypto-dash/src/pages/coin-details.jsx
+++ crypto-dash/src/pages/coin-details.jsx
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react";
-import {
-  Link,
-  UNSAFE_getTurboStreamSingleFetchDataStrategy,
-  useParams,
-} from "react-router";
+import { Link, useParams } from "react-router";
 import Spinner from "../components/Spinner";
+import CoinChart from "../components/CoinChart";
 
 const API_URL = import.meta.env.VITE_COIN_API_URL;
 
@@ -88,6 +85,8 @@ const CoinDetailsPage = () => {
               Last Updated: {new Date(coin.last_updated).toLocaleDateString()}
             </h4>
           </div>
+
+          <CoinChart coinId={coin.id} />
 
           <div className="coin-details-links">
             {coin.links.homepage[0] && (
